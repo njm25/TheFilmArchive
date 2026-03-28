c664c9f8e3d04db3314408da83c6d873491f3652
nicomilette <nicomilette7@gmail.com>
2026-03-28 16:51:02 -0400

    feat: run migration on prod startup

--- apps/api/Program.cs
+++ apps/api/Program.cs
@@ -88,6 +88,13 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+if (app.Environment.IsProduction())
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
