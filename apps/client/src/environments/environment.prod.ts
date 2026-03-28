b0b36a43a082929264269642296f2812eba1fd08
nicomilette <nicomilette7@gmail.com>
2026-03-28 03:50:44 -0400

    chore: another dumb typo

--- apps/client/src/environments/environment.prod.ts
+++ apps/client/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
 	production: true,
-	apiUrl: 'https://api.thefilmarchive.com',
+	apiUrl: 'https://api.thefilmarchive.org',
     bucketUrl: 'https://d1wwpf11v1dnfp.cloudfront.net'
 };
