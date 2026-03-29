4e891231a4a83ee0d93add618a93f7fe88e272b1
nicomilette <nicomilette7@gmail.com>
2026-03-29 14:54:44 -0400

    feat: toastr provider + error interceptor

---
+++ apps/client/src/app/interceptors/error.interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, throwError } from 'rxjs';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+	const toastr = inject(ToastrService);
+
+	return next(req).pipe(
+		catchError((error) => {
+			// Handle based on status
+			if (error.status !== 200) {
+				const message =
+					error?.error?.message ||
+					error?.statusText ||
+					'An unexpected error occurred';
+
+				toastr.error(message, `Error ${error.status}`);
+			}
+
+			return throwError(() => error);
+		})
+	);
+};
